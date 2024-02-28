6cced30abc21a50f35550feef0abf82b185c6687
Alex <alex@updf.cn>
2024-02-28 20:09:35 +0800

    [update] create project.

---
+++ app/src/main/java/io/github/alexmofer/android/support/app/MainActivity.java
@@ -0,0 +1,16 @@
+package io.github.alexmofer.android.support.app;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import io.github.alexmofer.android.support.app.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(ActivityMainBinding.inflate(getLayoutInflater()).getRoot());
+    }
+}
